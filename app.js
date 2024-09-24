9e9cfa57ebf37592646729e153ccd860f77ab558
danielwilberforce91 <44928150+danielwilberforce91@users.noreply.github.com>
2024-09-25 09:01:06 +1000

    Create app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+//Repo: https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
+
+
+const container = document.querySelector('#container');
+const baseUrl = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+
+for (let i = 1; i <= 1025; i++) {
+    const pokemon = document.createElement('div');
+    pokemon.classList.add('pokemon');
+    const label = document.createElement('span');
+    label.innerText = `#${i}`;
+    const newImg = document.createElement('img');
+    newImg.src = `${baseUrl}${i}.png`
+    
+    pokemon.appendChild(newImg);
+    pokemon.appendChild(label);
+    container.appendChild(pokemon);
+}
